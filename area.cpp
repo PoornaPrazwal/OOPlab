9289270600ae4e1f8d275e7f0c356645c83c1d68
PoornaPrazwal <159806882+PoornaPrazwal@users.noreply.github.com>
2024-02-21 21:32:56 +0530

    Update area.cpp

--- area.cpp
+++ area.cpp
@@ -1,33 +1,19 @@
-// main.cpp
 #include<iostream>
 #include "boxArea.h"
+#include<iostream>
 #include "boxVolume.h"
-
-int main() {
-    float length, width, height;
-
-    // Input from the user
-    std::cout << "Enter the length of the box: ";
-    std::cin >> length;
-
-    std::cout << "Enter the width of the box: ";
-    std::cin >> width;
-
-    std::cout << "Enter the height of the box: ";
-    std::cin >> height;
-
-    // Display area and volume
-    #ifdef BOX_AREA_H
-        boxArea(length, width, height);
-    #else
-        std::cout << "boxArea function not defined!" << std::endl;
-    #endif
-
-    #ifdef BOX_VOLUME_H
-        boxVolume(length, width, height);
-    #else
-        std::cout << "boxVolume function not defined!" << std::endl;
-    #endif
-
+#include "boxArea.h"
+using namespace std;
+int main()
+{
+    float l,b,h;
+    cout<<"Enter length of box: ";
+    cin>>l;
+    cout<<"Enter width of box: ";
+    cin>>b;
+    cout<<"Enter height of box: ";
+    cin>>h;
+    boxArea(l,b,h);
+    boxVolume(l,b,h);
     return 0;
 }
